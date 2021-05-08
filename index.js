95ca852e76ec88ab950334c4fc53e91009535570
Gideon B <Palison5331@gmail.com>
2021-05-08 13:31:31 +0100

    edited

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ $(window).scroll(function() {
     if (scroll >= 80) {
         //clearHeader, not clearheader - caps H
         $(".navbar").addClass("bg-light");
-        $(".nav-link").css("color","rgba(0,0,0,.55)");
+        $(".nav-link, .navbrand, .navbar-toggler").css("color","rgba(0,0,0,.55)");
+        // $(".navbrand").css("color","");
 
     } else {
       $(".navbar").removeClass("bg-light");
-      $(".nav-link").css("color","whitesmoke");
+      $(".nav-link, .navbrand, .navbar-toggler").css("color","whitesmoke");
+        // $(".navbrand").css("color","white");
 
     }
 }); //missing );
